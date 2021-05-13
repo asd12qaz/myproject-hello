d1a4017ebff500738ec2157f2bf3f302419a1934
asd12qaz <qwer7u@gmail.com>
2021-05-13 15:34:00 +0800

    scanner、system.in 用法

--- src/main/java/com/clay/student/Student.java
+++ src/main/java/com/clay/student/Student.java
@@ -1,8 +1,18 @@
 package com.clay.student;
 
+import java.util.Scanner;//雖然是內建類別，但不屬於java.lang的都需要ipport使用
+
 public class Student {
     public static void main(String[] args) {
-        StudentData sd =new StudentData("clay",60,90);
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("please enter name:");
+        String name=scanner.next();
+        System.out.println("please enter english:");
+        int english=scanner.nextInt();
+        System.out.println("please enter math:");
+        int math=scanner.nextInt();
+        StudentData sd =new StudentData(name,english,math);
         sd.printdata();
 
 
--- src/main/kotlin/com/clay/Student/Student.kt
+++ src/main/kotlin/com/clay/Student/Student.kt
@@ -1,7 +1,16 @@
 package com.clay.Student
 
+import java.util.*
+
 fun main() {
-    val sd=studentdata11("clay",60,90)
+    val scanner=Scanner(System.`in`)
+    println("please enter name:")
+    var name=scanner.next()
+    println("please enter english:")
+    var english=scanner.nextInt()
+    println("please enter math:")
+    var math=scanner.nextInt()
+    val sd=studentdata11(name,english, math)
     sd.print()
 }
 
@@ -9,7 +18,7 @@ class studentdata11(val name:String,val english:Int,val math:Int){
 
     fun print(){
 
-        println(name+"\t"+english+"\t"+math)
+        println(name+"\t"+english+"\t"+math+"\t"+average())
     }
     fun average():Int{
         var average =(english+math)/2
